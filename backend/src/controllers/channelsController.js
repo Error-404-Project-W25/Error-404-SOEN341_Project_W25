bc5260934cfec92a00765fa906d21f5845daa6da
Asley Kate Antonio <asleykantonio@gmail.com>
2025-01-29 20:08:51 -0500

    channelsController.js and channels.js created

---
+++ backend/src/controllers/channelsController.js
@@ -0,0 +1,24 @@
+// Create a new channel
+export const createChannels = (req, res) => {
+    // STEP 1: get team ID (where channel will be created)
+        // const teamID = req ...
+
+    // STEP 2: check if user is admin?
+    
+    // STEP 3: get channel info
+        // const channelName = req ...
+
+    // STEP 4: create a new channel object with info and save to database 
+}
+
+// Add users to a channel
+export const addUsersToChannel = (req, res) => {
+    // STEP 1: get channel ID (where users will be added)
+
+    // STEP 2: check if user is admin?
+
+    // STEP 3: check if desired user is part of the TEAM
+        // if yes, proceed. if no, keep prompting
+
+    // STEP 4: add user to channel database
+}
