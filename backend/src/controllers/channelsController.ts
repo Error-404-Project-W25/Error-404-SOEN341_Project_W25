bd1102e2d68629e210ee202193fe56d740e29846
Asley Kate Antonio <asleykantonio@gmail.com>
2025-01-30 22:26:01 -0500

    fixes to channelsController

--- backend/src/controllers/channelsController.ts
+++ backend/src/controllers/channelsController.ts
@@ -1,21 +1,20 @@
 import { Request, Response } from 'express';
 import { Channel } from '../models/channelsModel';
 import { Team } from '../models/teamsModel';
-import { User } from '../models/userModel'; // to add
+import { User, userSchema } from '../models/userModel'; // to add
 import { v4 as uuidv4 } from 'uuid';
 
 // Create a new channel 
-export const createChannel = async (req, res) => {
-    const team_id = req.params(); // get team that user is currently in
+export const createChannel = async (req: Request, res: Response): Promise<void> => {
+    const team_id = req.params; // get team that user is currently in
     const { channelName, channelDescription } = req.body; // get channel info
 
     try {
-        const team = await Team.findbyId(team_id); // look up the team by ID
+        const team = await Team.findById(team_id); // returns the team object
         if (!team) { // if not found
-            return res.status(404).json({ error: 'Team not found' });
+            res.status(404).json({ error: 'Team not found' });
+            return;
         } 
-
-        // fetch the admin from the team to initialize members
 
         const channel_id = uuidv4(); // unique identifier for channel id
 
@@ -27,9 +26,8 @@ export const createChannel = async (req, res) => {
             members: team.admin // admin is default member
         });
 
-        const savedTeam = await newChannel.save();
-        
-        return res.status(200).json({
+        const savedChannel = await newChannel.save();
+        res.status(201).json({
             message: 'The channel has been created successfully'
         });
 
@@ -44,22 +42,15 @@ export const createChannel = async (req, res) => {
 }
 
 // Add users to a channel
-export const addUserToChannel = async (req, res) => {
-    const channel_id = req.params(); // get channel id
-    const user_id = req.body(); // get name of the user to add
-    const team_id = req.params(); // get team that user is currently in
+export const addUserToChannel = async (req: Request, res: Response): Promise<void> => {
+    const channel_id = req.params; // get channel id that user is currently in
+    const user_id = req.body; // get name of the user to add
+    const team_id = req.params; // get team that user is currently in
 
     try {
-        const channel = Channel.findbyId(channel_id); // look up the channel by ID
-        if (!channel) { // if not found
-            return res.status(404).json({ error: 'Channel not found' });
-        } 
-
-        const team = Team.findbyId(team_id); // look up the team by ID
-        if (!team) { // if not found
-            return res.status(404).json({ error: 'Team not found' });
-        } 
-
+        const channel = Channel.findById(channel_id); // get the channel
+        const team = Team.findById(team_id); // get the team
+        
         // check if the user is part of the team
         if (!team.members.includes(user_id)) {
             res.status(400).json({ error: 'The user entered is not part of the team' });
@@ -72,11 +63,9 @@ export const addUserToChannel = async (req, res) => {
             return;
         }
 
-        // otherwise, add the user to the channel
+        // otherwise, add the user to the members of the channel
         channel.members.push(user_id);
-        await channel.save();
-
-        return res.status(200).json({
+        res.status(201).json({
             message: 'The user has been added to the channel successfully'
         });
 
