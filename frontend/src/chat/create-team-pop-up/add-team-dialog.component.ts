957834ed63611e2aefd6d86a69ae0cdc051be9c3
ZH_Tonny <thelonelyotaku@gmail.com>
2025-02-03 21:17:36 -0500

    refactor: integrate UserService to manage current user data in AddTeamDialogComponent

--- frontend/src/chat/create-team-pop-up/add-team-dialog.component.ts
+++ frontend/src/chat/create-team-pop-up/add-team-dialog.component.ts
@@ -1,5 +1,5 @@
+import { getUserInfo } from './../../../../backend/src/controllers/usersController';
 /* Create team Pop Up */
-
 import { Component } from '@angular/core';
 import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { MatInputModule } from '@angular/material/input';
@@ -7,6 +7,8 @@ import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { HttpClientModule } from '@angular/common/http';
 import { BackendService } from '../../services/backend.service';
+import { UserService } from '../../services/user.service';
+import { IUser } from '../../../../shared/interfaces';
 
 @Component({
   selector: 'app-add-team-dialog',
@@ -25,33 +27,37 @@ export class AddTeamDialogComponent {
   searchQuery = ''; // input from 'input matInput' is stored in searchQuery
   teamName = '';
   description = '';
+  teamMembers: IUser[] = [];
 
   constructor(
     private dialogRef: MatDialogRef<AddTeamDialogComponent>,
-    private backendService: BackendService
+    private backendService: BackendService,
+    private userService: UserService
   ) {}
 
   search() {
-    // when the button is clicked, the search function is called
-    console.log('searching for:', this.searchQuery);
-    // this.backendService.searchUser(this.searchQuery).then(
-    //   (response) => {
-    //     console.log('User found:', response);
-    //     // Add user to the team or perform any other action
-    //   },
-    //   (error) => {
-    //     console.error('Error searching for user:', error);
-    //   }
-    // );
   }
 
+  
+
   createTeam() {
+    const currentUser = this.userService.getUser();
+    if (!currentUser) {
+      console.error('No current user found');
+      return;
+    }
+
+    if (!currentUser.username) {
+      console.error('Current user does not have a username');
+      return;
+    }
+
     const teamData = {
-      user_id: 'exampleUserId', // Replace with actual user ID
-      username: 'exampleUsername', // Replace with actual username
+      user_id: currentUser.user_id,
+      username: currentUser.username,
       team_name: this.teamName,
       description: this.description,
-      members: [],
+      members: this.teamMembers,
       role: 'admin',
     };
 
