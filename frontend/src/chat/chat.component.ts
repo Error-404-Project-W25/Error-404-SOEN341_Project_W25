6e9d7cbacda544976ed3e825730b5f6b3f880f78
TheLonelyOtaku315 <thelonelyotaku@gmail.com>
2025-02-11 13:00:56 -0500

    Refactor channel creation dialog for improved validation and styling

--- frontend/src/chat/chat.component.ts
+++ frontend/src/chat/chat.component.ts
@@ -28,7 +28,21 @@ import { UserAuthResponse } from '../types/http-response.types';
 export class ChatComponent implements OnInit, OnDestroy {
   /*Test*/
   channelTitle: string = 'Channel Name';
+
   TeamTitle: string = 'Team Name';
+
+  teamMemberList: string[] = Array.from(
+    { length: 30 },
+    (_, i) => `Member ${i + 1}`
+  );
+
+  channelNameList: string[] = Array.from(
+    { length: 30 },
+    (_, i) => `Channel ${i + 1}`
+  );
+
+  teamList: string[] = Array.from({ length: 30 }, (_, i) => `T${i + 1}`);
+
   messages: Message[] = [
     new Message(
       'User3',
@@ -63,123 +77,19 @@ export class ChatComponent implements OnInit, OnDestroy {
     new Message('User2', '10:43 AM', 'Thanks! Let’s catch up next week.'),
     new Message('User1', '10:44 AM', 'Sounds good! Have a great weekend.'),
   ];
-  channelMemberList: string[] = [
-    'Member 1',
-    'Member 2',
-    'Member 3',
-    'Member 4',
-    'Member 5',
-    'Member 6',
-    'Member 7',
-    'Member 8',
-    'Member 9',
-    'Member 10',
-    'Member 11',
-    'Member 12',
-    'Member 13',
-    'Member 14',
-    'Member 15',
-    'Member 16',
-    'Member 17',
-    'Member 18',
-    'Member 19',
-    'Member 20',
-    'Member 21',
-    'Member 22',
-    'Member 23',
-    'Member 24',
-    'Member 25',
-    'Member 26',
-    'Member 27',
-    'Member 28',
-    'Member 29',
-    'Member 30',
-  ];
-  channelNameList: string[] = [
-    'Channel 1',
-    'Channel 2',
-    'Channel 3',
-    'Channel 4',
-    'Channel 5',
-    'Channel 6',
-    'Channel 7',
-    'Channel 8',
-    'Channel 9',
-    'Channel 10',
-    'Channel 11',
-    'Channel 12',
-    'Channel 13',
-    'Channel 14',
-    'Channel 15',
-    'Channel 16',
-    'Channel 17',
-    'Channel 18',
-    'Channel 19',
-    'Channel 20',
-    'Channel 21',
-    'Channel 22',
-    'Channel 23',
-    'Channel 24',
-    'Channel 25',
-    'Channel 26',
-    'Channel 27',
-    'Channel 28',
-    'Channel 29',
-    'Channel 30',
-  ];
-  teamList: string[] = [
-    'T1',
-    'T2',
-    'T3',
-    'T4',
-    'T5',
-    'T6',
-    'T7',
-    'T8',
-    'T9',
-    'T10',
-    'T11',
-    'T12',
-    'T13',
-    'T14',
-    'T15',
-    'T16',
-    'T17',
-    'T18',
-    'T19',
-    'T20',
-    'T21',
-    'T22',
-    'T23',
-    'T24',
-    'T25',
-    'T26',
-    'T27',
-    'T28',
-    'T29',
-    'T30',
-  ];
-  memberList: string[] = Array.from(
-    { length: 30 },
-    (_, i) => `Member ${i + 1}`
-  );
 
   /*End Test*/
 
+  /*variable*/
   teams: ITeam[] = [];
   channels: IChannel[] = [];
   selectedTeam: string | null = null;
   selectedChannel: string | null = null;
-  teamSelectedName = '';
-
   title = 'chatHaven';
 
+  /*Subscription */
   private teamCreatedSubscription: Subscription | null = null;
   private channelCreatedSubscription: Subscription | null = null;
-
-  /*private teamsSubject = new BehaviorSubject<ITeam[]>([]);
-  teams$ = this.teamsSubject.asObservable();
-  */
 
   private channelsSubject = new BehaviorSubject<IChannel[]>([]);
   channels$ = this.channelsSubject.asObservable();
@@ -187,197 +97,25 @@ export class ChatComponent implements OnInit, OnDestroy {
   private teamsSubject = new BehaviorSubject<ITeam[]>([]);
   teams$ = this.teamsSubject.asObservable();
 
+  /*Constructor */
   constructor(
     private router: Router,
     public dialog: MatDialog,
     private userService: UserService,
     private backendService: BackendService
-  ) {
-    this.refreshTeams();
-  }
+  ) {}
 
   /**
    * Lifecycle hook that is called after component initialization.
    * It initializes the teams array.
    */
-  ngOnInit() {
-    console.log('Chat component initialized');
-    this.userService.user$.subscribe((user: IUser | undefined) => {
-      if (user?.teams) {
-        this.refreshTeams();
-        // If there's a selected team, refresh channels
-        if (this.selectedTeam) {
-          this.refreshChannels();
-        }
-      }
-    });
-  }
+  ngOnInit() {}
 
   /**
    * Lifecycle hook that is called when the component is destroyed.
    * It unsubscribes from any active subscriptions to avoid memory leaks.
    */
-  ngOnDestroy() {
-    this.teamCreatedSubscription?.unsubscribe();
-    this.channelCreatedSubscription?.unsubscribe();
-  }
-
-  /**
-   * Opens a dialog to create a new channel and subscribes to its creation event.
-   */
-  openChannelDialog(): void {
-    const dialogRef = this.dialog.open(AddChannelDialogComponent, {
-      data: { selectedTeam: this.selectedTeam },
-    });
-    this.channelCreatedSubscription =
-      dialogRef.componentInstance.channelCreated.subscribe(
-        (newChannel: IChannel) => {
-          this.channels.push(newChannel);
-          this.onChannelCreated();
-        }
-      );
-  }
-
-  /**
-   * Opens a dialog to add a member to the team.
-   */
-  openAddMemberTeamDialog(): void {
-    const dialogRef = this.dialog.open(AddMemberTeamPopUpComponent, {
-      data: { selectedTeam: this.selectedTeam },
-    });
-    // Handle any actions after the dialog is closed, if needed
-    dialogRef.afterClosed().subscribe((result) => {
-      console.log('The dialog was closed');
-      // Perform any additional actions if needed
-    });
-  }
-
-  /**
-   * Opens a dialog to create a new team only if the user has admin privileges.
-   */
-  openTeamDialog(): void {
-    // if (this.userService.getUser()?.role === 'admin') {
-    const dialogRef = this.dialog.open(AddTeamDialogComponent);
-    this.teamCreatedSubscription =
-      dialogRef.componentInstance.teamCreated.subscribe(() => {
-        this.onTeamCreated();
-      });
-    // } else {
-    //   console.error('Permission denied: Only admins can create a team.');
-    //   alert('You do not have the permission to create a team');
-    // }
-  }
-
-  /**
-   * Called when a new team is created. It refreshes the teams list.
-   */
-  onTeamCreated() {
-    console.log('Team created, performing necessary actions...');
-    this.refreshTeams();
-  }
-
-  /**
-   * Called when a new channel is created. It refreshes the channels list.
-   */
-  onChannelCreated() {
-    console.log('Channel created, performing necessary actions...');
-    this.refreshChannels();
-  }
-
-  /**
-   * Updates the teams list from the current user data.
-   */
-  refreshTeams() {
-    const currentUser: IUser | undefined = this.userService.getUser();
-    if (currentUser) {
-      const teams = [...(currentUser.teams || [])];
-      this.teamsSubject.next(teams);
-      console.log('Teams updated:', teams);
-      if (teams.length > 0 && !this.selectedTeam) {
-        this.selectTeam(teams[0]);
-      }
-    }
-  }
-
-  /**
-   * Updates the channels list based on the selected team.
-   */
-  async refreshChannels() {
-    /*
-    if (this.selectedTeam) {
-      try {
-        const team = await this.backendService.getTeamById(this.selectedTeam);
-
-        if (!team) {
-          console.log('Team not found');
-          return;
-        }
-
-        const currentUser = this.userService.getUser();
-        if (!currentUser) return;
-
-        if (currentUser.role === 'admin') {
-          this.channelsSubject.next(team.channels);
-          console.log('Channels updated (admin):', team.channels);
-        } else {
-          const channels = team.channels.filter((c) =>
-            c.members.includes(currentUser.user_id ?? '')
-          );
-          this.channelsSubject.next(channels);
-          console.log('Channels updated (user):', channels);
-        }
-      } catch (error) {
-        console.error('Error refreshing channels:', error);
-      }
-    }
-      */
-  }
-
-  /**
-   * Selects a team and updates the available channels for that team.
-   */
-  async selectTeam(team: ITeam) {
-    this.selectedTeam = team.team_id ?? null;
-    this.teamSelectedName = team.team_name;
-    // Wait for channels to be refreshed before updating the subject
-    await this.refreshChannels();
-    console.log('You are inside the selectTeam function');
-    console.log('Selected Team:', team);
-  }
-
-  /**
-   * Selects a channel within the selected team.
-   */
-  selectChannel(channel: IChannel) {
-    this.selectedChannel = channel.name;
-    console.log('You are inside the selectChannel function');
-    console.log('Channel:', channel);
-  }
-
-  /**
-   * Logs out the current user and navigates back to the login page.
-   */
-  async signOut() {
-    const response: UserAuthResponse | undefined =
-      await this.backendService.logoutUser();
-
-    if (response && !response.error) {
-      console.log('Logging out from:', this.userService.getUser()?.username);
-      this.userService.clearUser();
-      this.router.navigate(['/']);
-    } else if (response) {
-      console.error(response.error);
-    } else {
-      console.error('No response from backend');
-    }
-  }
-
-  /**
-   * Placeholder function for menu selection.
-   */
-  selectSetting() {
-    console.log('You are inside the selectMenu function');
-  }
+  ngOnDestroy() {}
 }
 
 /**
