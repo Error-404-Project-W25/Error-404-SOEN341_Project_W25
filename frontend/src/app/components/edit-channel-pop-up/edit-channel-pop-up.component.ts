22c737900bafbd65f5c0879d4da5778657f6ddd8
likeblossom <pamanderzz@hotmail.com>
2025-02-13 18:55:16 -0500

    created channel settings skeleton

---
+++ frontend/src/app/components/edit-channel-pop-up/edit-channel-pop-up.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-channel-pop-up',
+  imports: [],
+  templateUrl: './edit-channel-pop-up.component.html',
+  styleUrl: './edit-channel-pop-up.component.css'
+})
+export class EditChannelPopUpComponent {
+
+}
