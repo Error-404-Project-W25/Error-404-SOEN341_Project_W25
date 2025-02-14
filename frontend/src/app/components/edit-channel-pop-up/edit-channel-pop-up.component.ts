a6f348289231da8da2d0f3ac458942a000f5933c
likeblossom <pamanderzz@hotmail.com>
2025-02-13 22:14:23 -0500

    edit channel import backend services

--- frontend/src/app/components/edit-channel-pop-up/edit-channel-pop-up.component.ts
+++ frontend/src/app/components/edit-channel-pop-up/edit-channel-pop-up.component.ts
@@ -7,6 +7,7 @@ import {
   MatDialogRef,
 } from '@angular/material/dialog';
 import { MatInputModule } from '@angular/material/input';
+import { BackendService } from '@services/backend.service';
 
 @Component({
   selector: 'app-edit-channel-pop-up',
@@ -25,23 +26,35 @@ import { MatInputModule } from '@angular/material/input';
 export class EditChannelPopUpComponent {
   channelName = '';
   description = '';
-
   constructor(
     public dialogRef: MatDialogRef<EditChannelPopUpComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any
+    private backendService: BackendService,
+    @Inject(MAT_DIALOG_DATA) public data: any,
   ) {
     // Initialize the form with the existing channel data
     this.channelName = data.name;
     this.description = data.description;
   }
 
-  updateChannel(): void {
+  /* Need backend to make the updateChannel() method */
+  async updateChannel(): Promise<void> {
     if (this.channelName && this.description) {
-      this.dialogRef.close({
-        name: this.channelName,
-        description: this.description
-      });
+      const success = await this.backendService.updateChannel(
+        this.data.team_id,
+        this.data.channel_id,
+        this.channelName,
+        this.description
+      );
+  
+      if (success) {
+        this.dialogRef.close({
+          name: this.channelName,
+          description: this.description
+        });
+      } else {
+        console.error('Failed to update channel');
+      }
     }
+      
   }
-
 }
