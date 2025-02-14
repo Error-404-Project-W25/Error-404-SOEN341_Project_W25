445154e8000fc85bbf94a818a24f9dbec1668b3d
Asley Kate Antonio <asleykantonio@gmail.com>
2025-02-14 15:24:30 -0500

    progress on unit tests for teamsController

--- backend/tests/teamsController.test.ts
+++ backend/tests/teamsController.test.ts
@@ -1,15 +1,38 @@
-
-import supertest from "supertest";
-import { Request, Response, NextFunction } from 'express';
-import { BackendService } from "../../frontend/src/services/backend.service";
-import { app } from '../src/app';
-const request = require('supertest');
-
+import { describe, test, it, expect } from '@jest/globals';
+import request from "supertest";
+import { app } from "../src/app";
 
 /*
     Testing Teams APIs
 */
 describe('teams', () => { 
+
+    
+   describe('getUserTeams', () => {
+    /*
+        Test Case 1: The user ID cannot be found
+    */
+        describe('given the user id cannot be found', () => {
+            it("should return a 500", async () => {
+                const userId = 'userId-doesnotexist-123' // random test userId
+                
+                const { statusCode } = await request(app).get(`/teams/user/${userId}`)
+                
+                expect( statusCode ).toBe( 500 );
+            })
+        })
+
+    /*
+        Test Case 2: The user ID can be found
+    */
+        describe('given the user id can be found', () => {
+            it("should return all teams of which the user is a member", async () => {
+                
+
+            })
+        })
+   })
+   
 
     /*
         Test getTeamById()
@@ -23,26 +46,32 @@ describe('teams', () => {
             it("should return a 404", async () => {
                 const teamId = 'teamId-doesnotexist-123' // random test teamId
                 
-                await request(app)
-                    .get(`/teams/getTeamById/${teamId}`)
-                    .expect(404);
+                const { statusCode } = await request(app).get(`/teams/getTeamById/${teamId}`)
+                
+                expect( statusCode ).toBe( 404 );
             });
         });
 
         /*
             Test Case 2: The team ID exists
-        
+        */
        describe('given the team does exist', () => {
-            it("should return a 200 status and the team", async () => {
-                //const team = await backendService.createTeam(testTeam);
-
+            it("should return a 200 status and the team object", async () => {
+                
                 const teamId = "7c4fe90a-4331-4ea8-9918-85ab499ee8fd";
+                const teamName = "team1";
 
-                const { body, statusCode } = await request(app)
-                    .get(`/teams/getTeamById/${teamId}`)
-                    .expect(200);
+                const res = await request(app).get(`/teams/getTeamById/${teamId}`)
+
+                console.log('Response:', res.body.status);
+                console.log('Status code:', res.body.team_name);
+                
+                expect( res.body.status ).toBe(200);
+                expect( res.body.teams.team_name ).toBe(teamName);
+                
             })
-       })
-            */
+       });
+            
     });
+
 });
