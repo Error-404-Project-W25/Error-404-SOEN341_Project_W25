eebfb0e4ed626e8785b6805d1b3b18530c4d3cac
fran-netizen <ysa_francesca@icloud.com>
2025-02-15 13:45:13 -0500

    Created a channel-settings component and team-settings component

---
+++ frontend/src/app/components/channel-settings/channel-settings.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-channel-settings',
+  imports: [],
+  templateUrl: './channel-settings.component.html',
+  styleUrl: './channel-settings.component.css'
+})
+export class ChannelSettingsComponent {
+
+}
