5c1d559c3bf1f89d8adfdaad812b429b579a2ee0
fran-netizen <ysa_francesca@icloud.com>
2025-02-15 20:16:35 -0500

    CSS remains a white bnackground with text, to be fixed

--- frontend/src/app/components/team-settings/team-settings.component.ts
+++ frontend/src/app/components/team-settings/team-settings.component.ts
@@ -12,7 +12,8 @@ import { BackendService } from '@services/backend.service';  // Make sure the se
 @Component({
   selector: 'app-team-settings',
   templateUrl: './team-settings.component.html',
-  styleUrls: ['./team-settings.component.css'],
+  styleUrls: [ './../../components/chat/chat.component.css',
+    './team-settings.component.css'],
   standalone: true,
   imports: [MatDialogModule, MatInputModule, FormsModule, MatButtonModule],
 })
@@ -20,15 +21,19 @@ export class TeamSettingsComponent {
   teamName = '';
   description = '';
 
+  isDarkTheme: boolean = false; // Dark theme by default
   constructor(
     public dialogRef: MatDialogRef<TeamSettingsComponent>,
     private backendService: BackendService,
-    @Inject(MAT_DIALOG_DATA) public data: any
+    @Inject(MAT_DIALOG_DATA)
+    public data: { name: string; description: string; theme: boolean }
   ) {
     // Initialize the form with the existing team data
     this.teamName = data.name;
     this.description = data.description;
+    this.isDarkTheme = data.theme; // Set the theme based on the injected data
   }
+
 
   // Update team settings (could interact with the backend service)
   /*
