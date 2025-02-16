7b4065ffff755b23638e80d986b3fcd3a02632ca
TheLonelyOtaku315 <thelonelyotaku315@gmail.com>
2025-02-16 14:08:52 -0500

    improve member list display, and remove unnecessary console logs

--- frontend/src/app/components/chat/chat.component.ts
+++ frontend/src/app/components/chat/chat.component.ts
@@ -72,26 +72,19 @@ export class ChatComponent implements OnInit, OnDestroy {
     window.addEventListener('resize', this.handleResize.bind(this));
     this.handleResize(); // Initial check
 
-    console.log('Chat component initialized');
-
     // Fetch user from the service
     this.loginUser = this.userService.getUser() || null;
-    console.log('Logged-in user (initial):', this.loginUser);
 
     // If user is not already set, subscribe to userService updates
     if (!this.loginUser) {
       this.userService.user$.subscribe((user) => {
         this.loginUser = user || null;
-        console.log('Logged-in user (from subscription):', this.loginUser);
         this.refreshTeamList();
       });
     }
-    // console.log('Theme:', this.currentTheme);
-
     this.userService.user$.subscribe((user: IUser | undefined) => {
       if (user) {
         this.teamList = user.teams;
-        console.log(user.teams);
       }
     });
   }
@@ -120,12 +113,10 @@ export class ChatComponent implements OnInit, OnDestroy {
   /*Toggle Theme */
   toggleTheme() {
     this.isDarkTheme = !this.isDarkTheme;
-    console.log('Theme:', this.isDarkTheme ? 'dark' : 'light');
   }
 
   /*Open Different Dialogue */
   openCreateTeamDialog(): void {
-    console.log('Inside function create team');
     if (this.loginUser?.role !== 'admin') {
       alert('You do not have the necessary permissions to create a team.');
       return;
@@ -140,14 +131,11 @@ export class ChatComponent implements OnInit, OnDestroy {
         });
         this.refreshTeamList();
         this.selectTeam(result.team_id);
-      } else {
-        console.log('No team created or team ID not returned.');
       }
     });
   }
 
   openCreateChannelDialog(): void {
-    console.log('Inside function create channel');
     this.dialog.open(AddChannelDialogComponent, {
       data: { selectedTeam: this.selectedTeamId, theme: this.isDarkTheme },
     });
@@ -164,7 +152,6 @@ export class ChatComponent implements OnInit, OnDestroy {
   }
 
   openRemoveMemberTeamDialog(): void {
-    console.log('Inside function remove team member');
     this.dialog.open(RemoveMemberTeamPopUpComponent, {
       data: { selectedTeam: this.selectedTeamId, theme: this.isDarkTheme },
     });
@@ -195,40 +182,37 @@ export class ChatComponent implements OnInit, OnDestroy {
 
     dialogRef.afterClosed().subscribe((result) => {
       if (result?.added) {
-        console.log('Members added:', result.members);
       }
     });
   }
 
   /*Select Different Team, Channel, Conversation */
   selectTeam(team: string): void {
-    console.log(
-      'selected team:',
-      this.teamList.find((t) => t.team_id === team) || 'not found'
-    );
     let teamMemberListID: string[] = [];
+    this.teamMemberList = [];
     this.selectedTeamId = team;
     this.backendService.getTeamById(team).then((teamData) => {
-      this.channelList = teamData?.channels || [];
-      this.teamTitle = teamData?.team_name || '';
-      teamMemberListID = teamData?.members || [];
-    });
-    // Fetch each user by their ID and populate the teamMemberList
-    this.teamMemberList = [];
-    teamMemberListID.forEach((userId) => {
-      this.backendService.getUserById(userId).then((userData) => {
-        if (userData) {
-          this.teamMemberList.push(userData);
-        }
+      if (teamData) {
+        this.channelList = teamData.channels || [];
+        this.teamTitle = teamData.team_name || '';
+        teamMemberListID = teamData.members || [];
+      }
+
+      teamMemberListID.forEach((member) => {
+        this.backendService.getUserById(member).then((userData) => {
+          if (userData) {
+            this.teamMemberList.unshift(userData);
+          }
+        });
       });
+
+      this.channelTitle = '';
+      this.handleResize();
     });
-    this.channelTitle = '';
-    this.handleResize();
   }
 
   selectChannel(channel: string): void {
     this.messages = [];
-    console.log('Selected channel:', channel);
     this.selectedChannel = channel;
     this.backendService
       .getChannelById(this.selectedTeamId!, channel)
@@ -239,7 +223,6 @@ export class ChatComponent implements OnInit, OnDestroy {
   }
 
   selectConversation(conversation: string): void {
-    console.log('Selected conversation:', conversation);
     // this.selectedTeam = conversation;
   }
 
@@ -253,7 +236,6 @@ export class ChatComponent implements OnInit, OnDestroy {
         this.newMessage
       )
     );
-    console.log('Sending message:', this.newMessage);
     this.newMessage = '';
   }
 
@@ -262,7 +244,6 @@ export class ChatComponent implements OnInit, OnDestroy {
     const response: UserAuthResponse | undefined =
       await this.backendService.logoutUser();
     if (response && !response.error) {
-      console.log('Logging out from:', this.userService.getUser()?.username);
       this.userService.clearUser();
       this.router.navigate(['/']);
     } else if (response) {
@@ -287,7 +268,6 @@ export class ChatComponent implements OnInit, OnDestroy {
   }
 
   openEditChannelDialog(channel: IChannel): void {
-    console.log('Opening edit channel dialog for:', channel.name);
     const dialogRef = this.dialog.open(EditChannelPopUpComponent, {
       data: {
         name: channel.name,
@@ -323,9 +303,7 @@ export class ChatComponent implements OnInit, OnDestroy {
 
   // Handle window resize events
   handleResize() {
-    console.log('handleResize called');
     const width = window.innerWidth;
-    console.log('Window width:', width);
 
     const sideBarOne = document.getElementById('side-bar-1');
     const sideBarTwo = document.getElementById('side-bar-2');
@@ -339,32 +317,26 @@ export class ChatComponent implements OnInit, OnDestroy {
 
     if (width <= 450) {
       displayStyle(cardContainer, 'none');
-      console.log('card-container hidden');
       if (this.selectedChannel) {
         displayStyle(sideBarOne, 'none');
         displayStyle(sideBarTwo, 'none');
         displayStyle(chatLog, 'flex');
-        console.log('chatLog shown');
       } else {
         displayStyle(sideBarOne, 'flex');
         displayStyle(sideBarTwo, 'flex');
         displayStyle(chatLog, 'none');
-        console.log('chatLog hidden');
       }
     } else {
       displayStyle(cardContainer, 'flex');
       displayStyle(sideBarOne, 'flex');
       displayStyle(sideBarTwo, 'flex');
       displayStyle(chatLog, 'flex');
-      console.log('chatLog shown');
     }
 
     if (width <= 1025) {
       displayStyle(teamListSettingBar, 'none');
-      console.log('teamListSettingBar hidden');
     } else {
       displayStyle(teamListSettingBar, 'flex');
-      console.log('teamListSettingBar shown');
     }
   }
 }
