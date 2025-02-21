227af00594bcf573204571fc2e68428e432430d6
Asley Kate Antonio <asleykantonio@gmail.com>
2025-02-21 13:46:09 -0500

    configure lint to warn unsused-vars and not error

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -13,7 +13,8 @@ export default [
     },
     rules: {
       "@typescript-eslint/no-explicit-any": "off", // Disable this rule
-      "unused-vars": "warn" // unused variables will be warnings and not errors
+      "no-unused-vars": "off", // turn off default rule
+      "@typescript-eslint/no-unused-vars": "warn" // unused variables will be warnings and not errors
     },
   },
 ];
