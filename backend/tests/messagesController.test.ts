e77bea8a220a69d61f07e1f200f69844b671efd1
fran-netizen <ysa_francesca@icloud.com>
2025-03-03 23:09:55 -0500

    made structure for messagesController.test.ts beforeAll to start server afterAll to delete test message

--- backend/tests/messagesController.test.ts
+++ backend/tests/messagesController.test.ts
@@ -6,3 +6,51 @@ import { Express } from 'express';
 /*
     === Testing Messages APIs ===
 */
+describe('messages', () => {
+
+  /*
+      Run the app before running the tests
+  */
+  let MessageIdToDelete: string | undefined;
+  let server: Express;
+
+  beforeAll(async () => {
+    server = app as Express;
+    await startServer();
+  });
+
+  /*
+      Cleanup after the tests:
+      Delete the message that was created during sendMessage.
+  */
+  afterAll(async () => {
+    try {
+      if (MessageIdToDelete) {
+        await request(server).delete('/messages')
+          .send({
+            conversationId: "JEST-CONVERSATION-123",
+            messageId: MessageIdToDelete
+          });
+        console.log("Message deleted in afterAll, id: ", MessageIdToDelete);
+      }
+    } catch (error) {
+      console.error("Error: ", error);
+    }
+  });
+
+  /*
+      === Test sendMessage() ===
+  */
+
+
+  /*
+          === Test deleteMessage() ===
+      */
+
+
+  /*
+      === Test getMessages() ===
+  */
+
+
+});
