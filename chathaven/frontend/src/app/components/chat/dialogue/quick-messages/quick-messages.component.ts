5c5b011d32b2ad716960e5495d3bb4c6b3cbd611
fran-netizen <ysa_francesca@icloud.com>
2025-04-06 22:39:28 -0400

    created quick messages component

---
+++ chathaven/frontend/src/app/components/chat/dialogue/quick-messages/quick-messages.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quick-messages',
+  imports: [],
+  templateUrl: './quick-messages.component.html',
+  styleUrl: './quick-messages.component.css'
+})
+export class QuickMessagesComponent {
+
+}
